8137802568dae21f24f9b7faf7a22404752eb199
Nada-Salah-Abizeid <nada.salah.it@gmail.com>
2025-05-13 00:31:37 +0300

    add middlewares to candidate

--- routes/web.php
+++ routes/web.php
@@ -7,21 +7,26 @@ use App\Http\Controllers\JobController;
 use App\Http\Controllers\ApplicationController;
 use App\Http\Controllers\ProfileController;
 
-Route::get('/jobs/search', [JobController::class, 'search'])->name('jobs.search');
-Route::get('/jobs/show/{id}', [JobController::class, 'show'])->name('jobs.show');
 
-Route::get('/applications', [ApplicationController::class, 'index'])->name('applications.index');
-Route::post('/applications', [ApplicationController::class, 'store'])->name('applications.store');
-Route::delete('/applications/{id}', [ApplicationController::class, 'destroy'])->name('applications.destroy');
+Route::middleware('auth')->group(function () {
+    Route::get('/jobs/search', [JobController::class, 'search'])->name('jobs.search');
+    Route::get('/jobs/show/{id}', [JobController::class, 'show'])->name('jobs.show');
+
+    Route::get('/applications', [ApplicationController::class, 'index'])->name('applications.index');
+    Route::post('/applications', [ApplicationController::class, 'store'])->name('applications.store');
+    Route::delete('/applications/{id}', [ApplicationController::class, 'destroy'])->name('applications.destroy');
 
 
-Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::post('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
+
+});
 
 
 Route::get('/notifications', function () {
-    return view('notifications.index'); })->middleware('auth');
+    return view('notifications.index');
+})->middleware('auth');
 
 
 Route::get('/', function () {
